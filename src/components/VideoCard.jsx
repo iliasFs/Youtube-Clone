5971e4fa2b6eb8bd1853b91038e8627f85dcec0b
iliasFs <110726288+iliasFs@users.noreply.github.com>
2023-03-20 12:24:46 +0200

    Videos implemented

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,35 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Typography, Card, CardContent, CartMedia, CardMedia } from "@mui/material";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
-import {
-  demoThumbnailUrl,
-  demoVideoUrl,
-  demoVideoTitle,
-  demoChannelUrl,
-  demoChannelTitle,
-} from "../utils/constants";
 
-const VideoCard = ({ video:{id:{videoId},snippet} }) => {
- 
- return ( 
- 
- <Card>
-  <Link to = {videoId? `/video/${videoId}` : null}>
-  <CardMedia 
-  image={snippet?.thumbnails?.high?.url}
-  alt = {snippet?.title}
-  sx = {{width:300, height:180}}
-  
-  
-  
-  />
-  </Link>
- </Card>
- 
- 
- 
- )
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card sx={{ width: { md: "320px", xs: "100%" }, boxShadow:'none', borderRadius:0 }}>
+      <Link to={videoId ? `/video/${videoId}` : null}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: "358px", height: "180px" }}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : null}>
+          <Typography
+            variant="subtitle1"
+            fontWeight="bold"
+            color="#FFF"
+            backgroundColor="#1e1e1e"
+          >
+            {snippet?.title.slice(0, 60) || null}
+          </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : null}>
+          <Typography
+            variant="subtitle2"
+            fontWeight="bold"
+            color="gray"
+            backgroundColor="#1e1e1e"
+          >
+            {snippet?.channelTitle.slice(0, 60) || null}
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default VideoCard;
